679dc76e3ef4aa8068ec395bb132420acbf68546
MrLagSux <paul.haupt@gmx.net>
2023-01-17 22:56:44 +0100

    Optimized generated Code
    - Compiler generates better code
    - Static Equations like "1 2 +" are evaluated before compilation
    - Added Help Menu

--- haupt.py
+++ haupt.py
@@ -2,6 +2,7 @@ from enum import Enum, auto
 from typing import List
 import subprocess
 import sys
+import os
 
 import colorama
 from colorama import Fore, Style
@@ -11,24 +12,24 @@ colorama.init()
 
 class OpSet(Enum):
     NOP = auto()
-    GET_MEM = auto()
-    SET_MEM = auto()
     PUSH = auto()
-    PRINT = auto()
     ADD = auto()
     SUB = auto()
     MUL = auto()
     DIV = auto()
     MOD = auto()
+    LT = auto()
+    GT = auto()
+    EQ = auto()
+    NEQ = auto()
+    PRINT = auto()
+    GET_MEM = auto()
+    SET_MEM = auto()
     IF = auto()
     ELSE = auto()
     DO = auto()
     END = auto()
     WHILE = auto()
-    EQ = auto()
-    NEQ = auto()
-    LT = auto()
-    GT = auto()
 
 
 GET_MEM_CHAR = "&"
@@ -88,7 +89,7 @@ def parse_op(op: str):
         op = OpSet.MOD,
     elif op == "print":
         op = OpSet.PRINT,
-    elif op.isdigit():
+    elif (op.startswith("-") and op[1:].isdigit()) or op.isdigit():
         op = OpSet.PUSH, int(op)
     elif op.startswith(SET_MEM_CHAR):
         op = OpSet.SET_MEM, op[len(SET_MEM_CHAR):]
@@ -113,7 +114,7 @@ def parse_op(op: str):
     elif op == "while":
         op = OpSet.WHILE,
     else:
-        print_error(f"Unknown token in parse_op(op)! Found",
+        print_error(f"Unknown token in parse_op(op)!",
                     f"{other_info[0]}:"
                     f"{Fore.CYAN}{other_info[1]}:{other_info[2]}{Style.RESET_ALL}"
                     f" => `{op}`")
@@ -122,6 +123,78 @@ def parse_op(op: str):
 
 def parse_instructions(code: List):
     return [parse_op(op) for op in code]
+
+
+def evaluate_static_equations(instructions):
+    # optimizes instructions like "10 2 1 3 * 4 5 * + - 2 * 7 - + 5 * 15"
+    # by evaluating them in the pre-compiler phase and only pushing the result
+    assert len(OpSet) == 19, "Make sure that `stack_op` in" \
+                             "`evaluate_static_equations()` is up to date."
+    # Last OP in our instruction set that is arithmetic
+    # All Enum values less than that are available to be pre-evaluated
+    last_arith = OpSet.MOD
+    new_code = []
+    instr_stack = []
+    for op in instructions:
+        instr = op[3]
+        if instr[0].value <= last_arith.value:
+            instr_stack.append(op)
+        else:
+            if len(instr_stack) > 0:
+                push_op = 0
+                math_op = 0
+                for s in instr_stack:
+                    if s[3][0] == OpSet.PUSH:
+                        push_op += 1
+                    else:
+                        math_op += 1
+                if push_op != math_op + 1:
+                    # Because all Math OP take pop 2 values and pop 1 value
+                    # You need 1 more PUSH than MATH op
+                    # If that's not the case, e.g. [*, *], just push them to output
+                    # They might be used for variables or loops
+                    for s in instr_stack:
+                        new_code.append(s)
+                    instr_stack = []
+                else:
+                    stack = []
+                    for s in instr_stack:
+                        stack_op = s[3]
+                        # print(f"  {s}")
+                        if stack_op[0] == OpSet.PUSH:
+                            stack.append(stack_op[1])
+                        elif stack_op[0] == OpSet.ADD:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(a + b)
+                        elif stack_op[0] == OpSet.SUB:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(b - a)
+                        elif stack_op[0] == OpSet.MUL:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(a * b)
+                        elif stack_op[0] == OpSet.DIV:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(int(b / a))
+                        elif stack_op[0] == OpSet.MOD:
+                            a = stack.pop()
+                            b = stack.pop()
+                            stack.append(int(b % a))
+                    result = stack.pop()
+                    # print("Result: " + str(result))
+                    last_instr = instr_stack[-1]
+                    # print(f"Last instruction: {last_instr}")
+                    new_instr = (last_instr[0], last_instr[1], last_instr[2], (OpSet.PUSH, result))
+                    instr_stack = []
+                    # print(f"Sequence breaker: {op}")
+                    # print("*********************")
+                    new_code.append(new_instr)
+            new_code.append(op)
+
+    return new_code
 
 
 def cross_reference(instructions):
@@ -184,11 +257,24 @@ def cross_reference(instructions):
                     f"{Fore.CYAN}{op_info[1]}:{op_info[2]}{Style.RESET_ALL}"
                     f" => `{op_info[3][0].name.lower()}` has no matching end.")
 
-    return instructions
+    # contains all indexes which will be jumped to
+    jmp_instr = []
+    for i, op in enumerate(instructions):
+        i = i + 1
+        instr = op[3]
+        if instr[0] == OpSet.ELSE:
+            jmp_instr.append(instr[1] + i)
+        elif instr[0] == OpSet.DO:
+            jmp_instr.append(instr[2] + i)
+        elif instr[0] == OpSet.END:
+            if instr[1] != 1:
+                jmp_instr.append(instr[1] + i)
+
+    return instructions, jmp_instr
 
 
 def simulate_code(instructions):
-    assert len(OpSet) == 16, "Not all OP can be simulated yet"
+    assert len(OpSet) == 19, "Not all OP can be simulated yet"
     stack = []
     index = 0
     while index < len(instructions):
@@ -216,6 +302,11 @@ def simulate_code(instructions):
             b = stack.pop()
             stack.append(int(b / a))
             index += 1
+        elif op[0] == OpSet.MOD:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(int(b % a))
+            index += 1
         elif op[0] == OpSet.PRINT:
             print(stack.pop())
             index += 1
@@ -233,10 +324,20 @@ def simulate_code(instructions):
             b = stack.pop()
             stack.append(int(a == b))
             index += 1
+        elif op[0] == OpSet.NEQ:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(int(a != b))
+            index += 1
         elif op[0] == OpSet.LT:
             a = stack.pop()
             b = stack.pop()
             stack.append(int(b < a))
+            index += 1
+        elif op[0] == OpSet.GT:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(int(b > a))
             index += 1
         elif op[0] == OpSet.DO:
             a = stack.pop()
@@ -262,14 +363,13 @@ def get_vars(instructions):
 # Call Program: output.exe
 # You can combine those commands with && for a single cmd to do all of it
 # Measure speed: powershell Measure-Command { .\output.exe }
-def compile_code(instructions):
+def compile_code(instructions, labels, opt_flags: dict):
     assert len(OpSet) == 19, "Not all OP can be compiled yet"
-    # TODO: Optimize PUSH/POP stuff
     used_vars = get_vars(instructions)
 
     name = "output"
     label_name = "instr"
-    with open(name + ".asm", "w") as output:
+    with open(name + ".tmp", "w") as output:
         output.write("bits 64\n")
         output.write("default rel\n")
         output.write("segment .data\n")
@@ -294,7 +394,8 @@ def compile_code(instructions):
                 print(i, op[0].name)
             else:
                 print(i, op[0].name, op[1])
-            output.write(f"{label_name}_{i}:\n")
+            if i in labels:
+                output.write(f"{label_name}_{i}:\n")
             output.write(f"; -- {op} --\n")
             if op[0] == OpSet.PUSH:
                 output.write(f"  mov rax, qword {op[1]}\n")
@@ -373,7 +474,7 @@ def compile_code(instructions):
             elif op[0] == OpSet.IF or op[0] == OpSet.WHILE:
                 pass
             elif op[0] == OpSet.ELSE:
-                end_goal = i + op[1]
+                end_goal = i + op[1] + 1
                 output.write(f"  jmp {label_name}_{end_goal}\n")
             elif op[0] == OpSet.DO:
                 if op[1][0] == OpSet.IF or op[1][0] == OpSet.WHILE:
@@ -387,7 +488,7 @@ def compile_code(instructions):
                 if op[1] == 1:
                     pass
                 else:
-                    end_goal = i + op[1]
+                    end_goal = i + op[1] + 1
                     output.write(f"  jmp {label_name}_{end_goal}\n")
             elif op[0] == OpSet.SET_MEM:
                 output.write("  pop rax\n")
@@ -402,7 +503,86 @@ def compile_code(instructions):
         output.write(f"{label_name}_{len(instructions)}:\n")
         output.write("  xor rax, rax\n")
         output.write("  call ExitProcess\n")
-    print("File written")
+
+    print(f"[INFO] Generated {name}.tmp")
+
+    if opt_flags['-o']:
+        optimized = []
+        not_done = []
+        with open(f"{name}.tmp", "r") as unoptimized:
+            content = unoptimized.readlines()
+            unoptimized_line_count = len(content)
+            content = [line.removesuffix("\n") for line in content]
+            for i, line in enumerate(content):
+                if line.startswith(";"):
+                    optimized.append((i, line))
+                else:
+                    not_done.append((i, line))
+            content = []
+            for (i, op) in not_done:
+                if not (op.startswith("  push") or op.startswith("  pop")):
+                    optimized.append((i, op))
+                else:
+                    content.append((i, op))
+
+            not_done = []
+            content.sort(key=lambda tup: tup[0])
+            for i in range(len(content)):
+                index = content[i][0]
+                op = content[i][1]
+                instr = op.strip(" ").split(" ")
+                if instr[0] != "push" and instr[0] != "pop":
+                    optimized.append((index, op))
+                else:
+                    not_done.append((index, op))
+            index = 0
+            while index < len(not_done):
+                curr_line = not_done[index]
+                if index >= len(not_done) - 1:
+                    optimized.append((index, curr_line[1]))
+                    index += 1
+                else:
+                    next_line = not_done[index + 1]
+                    curr_line_index = curr_line[0]
+                    next_line_index = next_line[0]
+                    if next_line_index == curr_line_index + 2:
+                        curr_line_instr = curr_line[1].strip(" ")
+                        next_line_instr = next_line[1].strip(" ")
+                        if curr_line_instr.startswith("push") and next_line_instr.startswith("pop"):
+                            reg = curr_line_instr.split(" ")[-1]
+                            if next_line_instr.endswith(reg):
+                                # push rax
+                                # pop  rax
+                                # useless OP
+                                pass
+                            else:
+                                optimized.append((curr_line_index, curr_line[1]))
+                                optimized.append((next_line_index, next_line[1]))
+                            index += 2
+                        else:
+                            optimized.append((curr_line_index, curr_line[1]))
+                            optimized.append((next_line_index, next_line[1]))
+                            index += 2
+                    else:
+                        optimized.append((curr_line_index, curr_line[1]))
+                        index += 1
+
+            optimized.sort(key=lambda tup: tup[0])
+            optimized_line_count = len(optimized)
+
+        print(f"[INFO] Removed {unoptimized_line_count - optimized_line_count} "
+              f"lines of ASM due to optimization.")
+        with open(f"{name}.asm", "w") as output:
+            for (i, op) in optimized:
+                output.write(op + "\n")
+    else:
+        with open(f"{name}.tmp", "r") as code:
+            with open(f"{name}.asm", "w") as asm:
+                asm.writelines(code.readlines())
+
+    print(f"[INFO] Generated {name}.asm")
+    os.remove(f"{name}.tmp")
+    print(f"[INFO] Removed {name}.tmp")
     call_cmd(["nasm", "-f", "win64", f"{name}.asm", "-o", f"{name}.obj"])
     call_cmd(["golink", "/no", "/console", "/entry", "main", f"{name}.obj", "MSVCRT.dll", "kernel32.dll"])
     print(f"[CMD] Created {name}.exe")
@@ -413,42 +593,72 @@ def shift(argv):
     return argv[0], argv[1:]
 
 
+def get_help(flag):
+    match flag:
+        case '-s': return "Simulates the given input code in Python."
+        case '-c': return "Compiles the given input code and generates a single executable."
+        case '-d': return "Debug Mode: Parses the input code, prints the instructions, then exits."
+        case '-o': return "Optimize the generated code. Only works in combination with `-c`."
+        case '-h': return "Shows this help screen."
+        case _: return "[No description]"
+
+
+def get_usage(program_name):
+    return f"Usage: {program_name} [-h] <input.hpt> [-s | -c | -d] [-o]"
+
+
 def main():
     # TODO: Add Strings, Arrays, Functions
-
+    flags = ['-s', '-c', '-d', '-o', '-h']
+    exec_flags = flags[:3]
+    optional_flags = flags[3:]
+    opt_flags = dict(zip(optional_flags, [False] * len(optional_flags)))
     program_name, sys.argv = shift(sys.argv)
     program_name = program_name.split("\\")[-1]
     if sys.argv[0] == '-h':
-        print(f"Usage: {program_name} <input.hpt> [-s | -c | -d]")
-        # TODO: Add Flag description
+        print(get_usage(program_name))
+        for flag in flags:
+            print(f"{flag}: " + get_help(flag))
         exit(0)
     if len(sys.argv) < 2:
         print_error("Not enough parameters!",
-                    f"Usage: {program_name} <input.hpt> [-s | -c | -d]\n"
+                    get_usage(program_name) + "\n"
                     f"       If you need more help, run `{program_name} -h`")
     input_file, sys.argv = shift(sys.argv)
     if not input_file.endswith(".hpt"):
         print_error(f"File {input_file} does not end with `.hpt`!",
-                    f"Usage: {program_name} <input.hpt> [-s | -c | -d]")
+                    get_usage(program_name))
     code = ""
     try:
         code = load_from_file(input_file)
     except FileNotFoundError:
         print_error(f"File `{input_file} does not exist!",
-                    f"Usage: {program_name} <input.hpt> [-s | -c | -d]")
-
-    instructions = parse_instructions(code)
-    # TODO: Optimize static equations,
-    #       move them to pre-compiler
-    instructions = cross_reference(instructions)
+                    get_usage(program_name))
 
     run_flag, sys.argv = shift(sys.argv)
+    if run_flag not in exec_flags:
+        print_error("Third Parameter has to be an execution flag!",
+                    get_usage(program_name))
+
+    instructions = parse_instructions(code)
+    instructions = evaluate_static_equations(instructions)
+    instructions, labels = cross_reference(instructions)
+
+    if len(sys.argv) > 0:
+        opt_args = sys.argv
+        for opt in opt_args:
+            if opt not in optional_flags:
+                print_error("Unknown Flag",
+                            f"Found `{opt}`. For valid flags run `{program_name} -h`")
+            else:
+                opt_flags[opt] = True
+
     if run_flag == '-s':
         print("Simulated Output: ")
         simulate_code(instructions)
     elif run_flag == '-c':
         print("Compiled Output: ")
-        compile_code(instructions)
+        compile_code(instructions, labels, opt_flags)
     elif run_flag == '-d':
         for i, op in enumerate(instructions):
             print(i, op)

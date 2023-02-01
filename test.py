26a0741d639128f8d56ac132bed8e6a6cbf5f8e2
MrLagSux <paul.haupt@gmx.net>
2023-02-01 19:54:59 +0100

    [Refactor] Improved Comparator Compilation
    Uses less push/pop now, instantly moves flags to register for further manipulation.
    Bugfix: GT operator was acting as GTE operator.
    Added execution time display in`test.py`.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os
 from typing import List
 import colorama
 from colorama import Fore, Style
+import time
 
 colorama.init()
 
@@ -16,10 +17,12 @@ def main():
     print("TESTING ALL PROJECT-EULER EXAMPLES.")
     print("If everything is fine, this code exits with exit code 0.")
     print("If not, you'll see shiny red text that shows what went wrong.")
+    total_time = 0
     for filename in os.listdir("./project-euler"):
         if filename.endswith(".hpt"):
             filename = "./project-euler/" + filename
             print("Running " + filename)
+            start_time = time.time()
             exitcode = call_cmd(["python", "haupt.py", filename, "-c", "-o", "-m"])
             print("Compiling " + filename + " exited with exit code " + str(exitcode))
             if exitcode != 0:
@@ -30,7 +33,12 @@ def main():
             if exitcode != 0:
                 print(Fore.RED + "EXECUTION FAILED: " + filename + Style.RESET_ALL)
                 exit(1)
+            end_time = time.time()
+            script_time = (end_time - start_time) * 1000
+            total_time += script_time
+            print("Script took {:.2f}ms.".format(script_time))
             print("*" * 100)
+    print("Testing took {:.2f}ms.".format(total_time))
     print(Fore.GREEN + "All Tests passed!" + Style.RESET_ALL)
 
 
